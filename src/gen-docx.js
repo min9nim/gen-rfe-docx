9b8852a076103852bb4a927ef59c6060b017f196
min9nim <min9nim@gmail.com>
2019-12-18 18:47:29 +0900

    동일한 json 이면 기존 생성했던 docx 리턴

--- src/gen-docx.js
+++ src/gen-docx.js
@@ -1,52 +1,63 @@
-var PizZip = require('pizzip');
-var Docxtemplater = require('docxtemplater');
+var PizZip = require("pizzip")
+var Docxtemplater = require("docxtemplater")
+var hash = require("object-hash")
+var fs = require("fs")
+var path = require("path")
 
-var fs = require('fs');
-var path = require('path');
+module.exports = function genDocx(json) {
+  const filename = hash(json) + ".docx"
+  const fullPath = "/tmp/" + filename
+  console.log("filename = " + filename, fullPath)
+  if (fs.existsSync(fullPath)) {
+    console.log("hit")
+    return filename
+  } else {
+    console.log("new json")
+  }
 
-module.exports = function genDocx(json){
   //Load the docx file as a binary
-  var content = fs
-      .readFileSync(path.resolve(__dirname, 'rfe_template.docx'), 'binary');
+  var content = fs.readFileSync(
+    path.resolve(__dirname, "rfe_template.docx"),
+    "binary",
+  )
 
-  var zip = new PizZip(content);
+  var zip = new PizZip(content)
 
-  var doc = new Docxtemplater();
-  doc.loadZip(zip);
+  var doc = new Docxtemplater()
+  doc.loadZip(zip)
 
   //set the templateVariables
-  doc.setData(json);
+  doc.setData(json)
 
   try {
-      // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
-      doc.render()
-  }
-  catch (error) {
-      // The error thrown here contains additional information when logged with JSON.stringify (it contains a properties object).
-      var e = {
-          message: error.message,
-          name: error.name,
-          stack: error.stack,
-          properties: error.properties,
-      }
-      console.log(JSON.stringify({error: e}));
-      if (error.properties && error.properties.errors instanceof Array) {
-          const errorMessages = error.properties.errors.map(function (error) {
-              return error.properties.explanation;
-          }).join("\n");
-          console.log('errorMessages', errorMessages);
-          // errorMessages is a humanly readable message looking like this :
-          // 'The tag beginning with "foobar" is unopened'
-      }
-      throw error;
+    // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
+    doc.render()
+  } catch (error) {
+    // The error thrown here contains additional information when logged with JSON.stringify (it contains a properties object).
+    var e = {
+      message: error.message,
+      name: error.name,
+      stack: error.stack,
+      properties: error.properties,
+    }
+    console.log(JSON.stringify({ error: e }))
+    if (error.properties && error.properties.errors instanceof Array) {
+      const errorMessages = error.properties.errors
+        .map(function(error) {
+          return error.properties.explanation
+        })
+        .join("\n")
+      console.log("errorMessages", errorMessages)
+      // errorMessages is a humanly readable message looking like this :
+      // 'The tag beginning with "foobar" is unopened'
+    }
+    throw error
   }
 
-  var buf = doc.getZip()
-              .generate({type: 'nodebuffer'});
+  var buf = doc.getZip().generate({ type: "nodebuffer" })
 
   // buf is a nodejs buffer, you can either write it to a file or do anything else with it.
-    const filename =  Date.now() + '.docx'
-    fs.writeFileSync('/tmp/' + filename, buf);      // now 배포환경에서는 /tmp/ 에만 파일쓰기 가능
-    return filename
-} 
-
+  // const filename =  Date.now() + '.docx'
+  fs.writeFileSync(fullPath, buf) // now 배포환경에서는 /tmp/ 에만 파일쓰기 가능
+  return filename
+}
